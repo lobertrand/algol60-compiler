ec0268312b7b9a8aefe848219ba393dd5c9c9ffd
Zineb Ziani El Idrissi <zineb.ziani-el-idrissi@telecomnancy.eu>
2020-02-09 01:17:52 +0100

    test IFStatement

--- src/test/java/eu/telecomnancy/semantic/IfStatementTest.java
+++ src/test/java/eu/telecomnancy/semantic/IfStatementTest.java
@@ -67,5 +67,42 @@ public class IfStatementTest {
         assertExceptionQuantity(0, result);
     }
 
+    @Test
+    public void test_IfTypeMismatch() throws RecognitionException {
+        Content c = new Content();
+        c.line("begin");
+        c.line("  real r;");
+        c.line("  if r  then x:");
+        c.line("   else b: ");
+        c.line("end");
+        Result result = checkSemantics(c);
+        assertExceptionQuantity(1, result);
+        assertExceptionAtLine(3, TypeMismatchException.class, result);
+    }
 
+    @Test
+    public void test_IfSymbolNotDeclared() throws RecognitionException {
+        Content c = new Content();
+        c.line("begin");
+        c.line("  real e;");
+        c.line("  if e <> a then x:");
+        c.line("   else b: ");
+        c.line("end");
+        Result result = checkSemantics(c);
+        assertExceptionQuantity(1, result);
+        assertExceptionAtLine(3, SymbolNotDeclaredException.class, result);
+    }
+
+    @Test
+    public void test_IfImbrique() throws RecognitionException {
+        Content c = new Content();
+        c.line("begin");
+        c.line("  integer n;");
+        c.line("  n := 3 ;");
+        c.line("  if n < 6 then ");
+        c.line("   if n > 2 then a: ");
+        c.line("end");
+        Result result = checkSemantics(c);
+        assertExceptionQuantity(0, result);
+    }
 }
