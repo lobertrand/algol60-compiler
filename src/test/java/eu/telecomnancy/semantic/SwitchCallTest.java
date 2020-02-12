5f5b685663ce6e0e268725e0307a5f34c65934fa
ZianiElIdrissi Zineb <zineb.ziani-el-idrissi@telecomnancy.eu>
2020-02-12 16:37:23 +0100

    merge et switchCallTest

---
+++ src/test/java/eu/telecomnancy/semantic/SwitchCallTest.java
@@ -0,0 +1,31 @@
+package eu.telecomnancy.semantic;
+
+import static eu.telecomnancy.semantic.Helper.*;
+
+import org.antlr.runtime.RecognitionException;
+import org.junit.Test;
+
+public class SwitchCallTest {
+
+    @Test
+    public void testSwitchBase() throws RecognitionException {
+        Content c = new Content();
+        c.line("begin");
+        c.line("  switch new := a,b,r ;");
+        c.line("   a: ;");
+        c.line(" r: ");
+        c.line(" b: ;");
+        c.line("integer q ;");
+        c.line("ininteger(q,6);");
+        c.line(" new[2]");
+        c.line("end");
+
+        Result result = checkSemantics(c);
+        assertExceptionQuantity(0, result);
+    }
+
+    @Test
+    public void testSwitch
+
+
+}
