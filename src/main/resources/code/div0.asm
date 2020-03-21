dd0c8c7b9eb186f62aaae16b9e9bdd40f96e1d2f
Loïc Bertrand <loic.bertrand@telecomnancy.eu>
2020-03-21 15:06:43 +0100

    Sauts de ligne après le message d'erreur

--- src/main/resources/code/div0.asm
+++ src/main/resources/code/div0.asm
@@ -1,8 +1,8 @@
-	// Print error message
-	LDW R0, #DIV0
+    // Print error message
+    LDW R0, #DIV0
     TRP #WRITE_EXC
     // Print newlines
-	LDW R0, #NEWLINE
+    LDW R0, #NEWLINE
     TRP #WRITE_EXC
     TRP #WRITE_EXC
     // Exit program
