4488a236683a4cca547e211eebca34aa916a65f8
Tony Zhou <tony.zhou@telecomnancy.eu>
2020-04-08 17:55:06 +0200

    Ajout début test arithm

---
+++ src/test/java/eu/telecomnancy/codegen/BasicArithmetic.java
@@ -0,0 +1,30 @@
+package eu.telecomnancy.codegen;
+
+import static eu.telecomnancy.codegen.Helper.Result;
+import static eu.telecomnancy.codegen.Helper.parse;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BasicArithmetic {
+
+    @Test
+    public void testBasicCalculus() throws Exception {
+        Result result =
+                parse(
+                        "begin integer a, b; a:=1; b:=2; outinteger(1, a+b);"
+                                + "outinteger(1, a-b);"
+                                + "outinteger(1, a*b);"
+                                + "outinteger(1, a//b);"
+                                + "outinteger(1, (b*b*b*b*b+a*(b*b+a)+b-a)//b) end");
+        String s = "+3\n-1\n+2\n+0\n+19";
+        assertEquals(s, result.output);
+    }
+
+    @Test
+    public void testPowPow() throws Exception {
+        Result result = parse("begin integer a, b; a:=2; b:=2;" + "outreal(1, a**2);" + "end");
+        String s = "+4";
+        assertEquals(s, result.output);
+    }
+}
