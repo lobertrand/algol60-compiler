255d266cb354d044dbe739ef04c0186455c0f15f
ZianiElIdrissi Zineb <zineb.ziani-el-idrissi@telecomnancy.eu>
2020-04-15 15:28:44 +0200

    test switch

---
+++ src/test/java/eu/telecomnancy/codegen/SwitchTest.java
@@ -0,0 +1,23 @@
+package eu.telecomnancy.codegen;
+
+import static eu.telecomnancy.codegen.Helper.Result;
+import static eu.telecomnancy.codegen.Helper.parse;
+import static org.junit.Assert.assertEquals;
+
+import eu.telecomnancy.tools.IOUtils;
+import org.junit.Test;
+
+public class SwitchTest {
+
+    @Test
+    public void testBasicSwitch() throws Exception {
+        Result result = parse(IOUtils.loadString("/codegen/unit_tests/switch.alg"));
+        assertEquals("first third", result.output);
+    }
+
+    @Test
+    public void testBasicSwitch2() throws Exception {
+        Result result = parse(IOUtils.loadString("/codegen/unit_tests/switch2.alg"));
+        assertEquals("second third", result.output);
+    }
+}
